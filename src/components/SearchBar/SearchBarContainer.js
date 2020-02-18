9a3f823437b247c6b4da82a51526f7854db227e8
Steven Wang <stevenwang2060@gmail.com>
2020-02-18 15:24:45 -0800

    Finshed MVP

--- src/components/SearchBar/SearchBarContainer.js
+++ src/components/SearchBar/SearchBarContainer.js
@@ -7,6 +7,7 @@ const SearchBar = () => {
     <div className="search-bar-wrapper">
       <div className="image-wrapper">
         <i className="fab fa-instagram" />
+        <h2>Instagram</h2>
       </div>
       <form className="search-form">
         <input
